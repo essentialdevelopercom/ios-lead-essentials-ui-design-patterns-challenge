ced3cbd54f94aab324adc05340340e3b097e5eeb
Ghenadie Pusca <pusca.ghenadie@gmail.com>
2020-01-15 15:37:39 +0200

    Move FeedErrorViewModel to its own file

---
+++ MVP Module/Source/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedErrorViewModel.swift
+//  MVPTests
+//
+//  Created by Pusca, Ghenadie on 1/15/20.
+//  Copyright © 2020 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+struct FeedErrorViewModel {
+    let errorMessage: String?
+}
