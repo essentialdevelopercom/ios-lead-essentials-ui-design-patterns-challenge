6d57c48fa00014b34d9bd7d75ce0a05d737e27f0
Ghenadie Pusca <pusca.ghenadie@gmail.com>
2020-01-15 14:24:02 +0200

    Error message is dismissed on user tap

---
+++ MVP Module/Tests/Feed UI/Helpers/ErrorView+SimulateDismiss.swift
@@ -0,0 +1,16 @@
+//
+//  ErrorView+SimulateDismiss.swift
+//  MVPTests
+//
+//  Created by Pusca, Ghenadie on 1/15/20.
+//  Copyright © 2020 Essential Developer. All rights reserved.
+//
+
+import UIKit
+import MVP
+
+extension ErrorView {
+    func simulateMessageDismiss() {
+        button.simulateTap()
+    }
+}
