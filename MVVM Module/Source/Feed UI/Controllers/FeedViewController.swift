3d860ee055731a619aef9f38ed9b90a2afc1ded7
Andrew <@users.noreply.github.com>
2020-04-10 12:47:39 +0100

    MVVM: Connecting storyboard with backing code

--- MVVM Module/Source/Feed UI/Controllers/FeedViewController.swift
+++ MVVM Module/Source/Feed UI/Controllers/FeedViewController.swift
@@ -19,6 +19,8 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
 		refresh()
 	}
 	
+    @IBOutlet private(set) public var errorView: ErrorView?
+    
 	@IBAction private func refresh() {
 		viewModel?.loadFeed()
 	}
