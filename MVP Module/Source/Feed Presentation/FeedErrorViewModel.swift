f20262caf3f5bfe17571b5a1288586481c92e145
Raphael Silva <sr.silvaraphael@gmail.com>
2020-06-30 01:00:27 +0200

    MVP: move `FeedErrorViewModel` to its own file.

---
+++ MVP Module/Source/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  FeedErrorViewModel.swift
+//  FeedFeature
+//
+//  Created by Raphael Silva on 30/06/2020.
+//  Copyright © 2020 Essential Developer. All rights reserved.
+//
+
+struct FeedErrorViewModel {
+    let message: String?
+}
