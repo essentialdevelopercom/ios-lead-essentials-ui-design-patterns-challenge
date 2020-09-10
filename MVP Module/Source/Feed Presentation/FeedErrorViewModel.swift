6a2e7b78d5c2b3844ba0364848697bbd992e61b8
J. Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-09-10 13:33:19 -0500

    Moves `FeedErrorViewModel` to its own file.

---
+++ MVP Module/Source/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  Created by Jesús Alfredo Hernández Alarcón on 10/09/20.
+//  Copyright © 2020 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+struct FeedErrorViewModel {
+    let message: String?
+}
