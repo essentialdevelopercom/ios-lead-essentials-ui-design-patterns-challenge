25c733ac15a470d08798b109610234b0d18136eb
Araceli Ruiz Ruiz <araruizalcuadrado@gmail.com>
2020-09-26 11:35:31 +0200

    MVP Move FeedErrorViewModel to its own file

---
+++ MVP Module/Source/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedErrorViewModel.swift
+//  FeedFeature
+//
+//  Created by Araceli Ruiz Ruiz on 26/09/2020.
+//  Copyright © 2020 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+struct FeedErrorViewModel {
+    let errorMessage: String?
+}
