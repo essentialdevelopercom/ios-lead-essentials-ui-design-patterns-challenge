edf54d57dfc2391893f7ce864475adabeeb81dfc
mulp <fabio.cuomo@gmail.com>
2020-11-09 16:15:55 +0100

    Show error message on feed load connectivity error

--- MVVM Module/Tests/Feed UI/Helpers/FeedViewController+TestHelpers.swift
+++ MVVM Module/Tests/Feed UI/Helpers/FeedViewController+TestHelpers.swift
@@ -57,4 +57,12 @@ extension FeedViewController {
 	private var feedImagesSection: Int {
 		return 0
 	}
+    
+    var errorMessage: String? {
+        errorView?.message
+    }
+    
+    private var errorView: ErrorView? {
+        tableView.tableHeaderView as? ErrorView
+    }
 }
