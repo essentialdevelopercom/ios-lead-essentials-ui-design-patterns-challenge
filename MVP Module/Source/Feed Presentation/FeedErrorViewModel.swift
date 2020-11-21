d2048f120fbcf51889e5c3ceba9e7df4a5b8bc8d
Rakesh Ramamurthy <rramamur@visa.com>
2020-11-21 14:06:48 +0530

    Moving 'FeedErrorViewModel' to its own file

---
+++ MVP Module/Source/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedErrorViewModel.swift
+//  MVP
+//
+//  Created by Rakesh Ramamurthy on 21/11/20.
+//  Copyright © 2020 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+struct FeedErrorViewModel {
+    let errorMessage: String?
+}
