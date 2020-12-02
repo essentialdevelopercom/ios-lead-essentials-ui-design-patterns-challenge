30f5b63cc994b1f76db3a55c1856c7d8101c93c9
Libor Polehna <polehna.libor@gmail.com>
2020-12-02 16:19:15 +0400

    MVP: Merged tests of error messages on feed error to one test that covers all use cases and ensures the correct flow of error messages when a user interacts with UI.

--- MVP Module/Tests/Feed UI/FeedUIIntegrationTests.swift
+++ MVP Module/Tests/Feed UI/FeedUIIntegrationTests.swift
@@ -77,27 +77,12 @@ final class FeedUIIntegrationTests: XCTestCase {
 		loader.completeFeedLoadingWithError(at: 1)
 		assertThat(sut, isRendering: [image0])
 	}
-    
-    func test_loadFeedCompletion_rendersNoErrorMessageOnInicialization() {
-        let (sut, _) = makeSUT()
-        
-        sut.loadViewIfNeeded()
-        XCTAssertEqual(sut.errorMessage, nil)
-    }
-    
-    func test_loadFeedCompletion_rendersErrorMessageOnError() {
-        let (sut, loader) = makeSUT()
-        
-        sut.loadViewIfNeeded()
-
-        loader.completeFeedLoadingWithError(at: 0)
-        XCTAssertEqual(sut.errorMessage, localized("FEED_VIEW_CONNECTION_ERROR"))
-    }
 
     func test_loadFeedCompletion_rendersErrorMessageOnErrorUntilNextReload() {
         let (sut, loader) = makeSUT()
         
         sut.loadViewIfNeeded()
+        XCTAssertEqual(sut.errorMessage, nil)
 
         loader.completeFeedLoadingWithError(at: 0)
         XCTAssertEqual(sut.errorMessage, localized("FEED_VIEW_CONNECTION_ERROR"))
