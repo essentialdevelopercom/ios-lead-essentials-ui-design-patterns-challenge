e776e415f0800331989a97fb72cfbe9d06a37099
Valentin Kalchev <valentin@zuant.com>
2020-12-08 19:43:15 +0000

    MVVM: Load feed completion renders error message on error until next reload.

--- MVVM Module/Tests/Feed UI/FeedUIIntegrationTests.swift
+++ MVVM Module/Tests/Feed UI/FeedUIIntegrationTests.swift
@@ -293,6 +293,20 @@ final class FeedUIIntegrationTests: XCTestCase {
         loader.completeFeedLoadingWithError()
         XCTAssertEqual(sut.errorMessage, localized("FEED_VIEW_CONNECTION_ERROR"))
     }
+    
+    
+    func test_loadFeedCompletion_rendersErrorMessageOnErrorUntilNextReload() {
+        let (sut, loader) = makeSUT()
+        
+        sut.loadViewIfNeeded()
+        XCTAssertEqual(sut.errorMessage, nil)
+
+        loader.completeFeedLoadingWithError(at: 0)
+        XCTAssertEqual(sut.errorMessage, localized("FEED_VIEW_CONNECTION_ERROR"))
+        
+        sut.simulateUserInitiatedFeedReload()
+        XCTAssertEqual(sut.errorMessage, nil)
+    }
 	
 	// MARK: - Helpers
 	
