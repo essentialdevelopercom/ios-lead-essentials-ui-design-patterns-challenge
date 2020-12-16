8576e609c5743e4dfcc3adc2c371e8e8e7e3a01b
Valentin Kalchev <valentin@zuant.com>
2020-12-16 11:11:05 +0000

    MVP: Removed tests for 'no error message on view load' and 'complete feed loading rendering error message on error' as these scenarios are tested in the 'load feed completion renders error message on error until next reload' test.

--- MVP Module/Tests/Feed UI/FeedUIIntegrationTests.swift
+++ MVP Module/Tests/Feed UI/FeedUIIntegrationTests.swift
@@ -279,20 +279,7 @@ final class FeedUIIntegrationTests: XCTestCase {
 			exp.fulfill()
 		}
 		wait(for: [exp], timeout: 1.0)
-	}
-    
-    func test_noErrorMessageOnViewLoad() {
-        let (sut, _) = makeSUT()
-        sut.loadViewIfNeeded()
-        XCTAssertNil(sut.errorMessage, "Error message is visible when view is loaded")
-    }
-    
-    func test_completeFeedLoading_rendersErrorMessageOnError() {
-        let (sut, loader) = makeSUT()
-        sut.loadViewIfNeeded()
-        loader.completeFeedLoadingWithError()
-        XCTAssertEqual(sut.errorMessage, localized("FEED_VIEW_CONNECTION_ERROR"))
-    }
+	} 
     
     func test_loadFeedCompletion_rendersErrorMessageOnErrorUntilNextReload() {
         let (sut, loader) = makeSUT()
