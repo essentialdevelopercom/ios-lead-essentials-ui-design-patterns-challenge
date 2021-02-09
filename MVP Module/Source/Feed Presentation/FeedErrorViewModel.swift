ddfb9f0ab472f0ae137303e5f3494d38375c0472
Marko Engelman <engleman.marko@gmail.com>
2021-02-09 23:52:50 +0100

    [MVP] Add 'FeedErrorViewModel'.

---
+++ MVP Module/Source/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedErrorViewModel.swift
+//  MVP
+//
+//  Created by Marko Engelman on 09/02/2021.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+struct FeedErrorViewModel {
+	let message: String?
+}
