4e142e1f828a4a2d77eed09c220f40658f8331c2
Muhammad Usman <usm.gh36@gmail.com>
2021-02-09 12:17:08 +0500

    Loading Feed Error in case of Load Failure, error view show error message

--- MVVM Module/Source/Feed UI/Controllers/FeedViewController.swift
+++ MVVM Module/Source/Feed UI/Controllers/FeedViewController.swift
@@ -34,6 +34,15 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
 				self?.refreshControl?.endRefreshing()
 			}
 		}
+		
+		viewModel?.onFeedError = { [weak self] error in
+			if let errorMessage = error {
+				self?.errorView?.show(message: errorMessage)
+			}
+			else{
+				self?.errorView?.hideMessage()
+			}
+		}
 	}
 	
 	public override func viewDidLayoutSubviews() {
