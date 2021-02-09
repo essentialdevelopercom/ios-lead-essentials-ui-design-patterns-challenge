6265bad4886d87c907c513ed54092c0f1c0a8edf
Marko Engelman <engleman.marko@gmail.com>
2021-02-09 21:24:34 +0100

    [MVVM] Dismiss 'errorMessage' on tap.

--- MVVM Module/Tests/Feed UI/Helpers/FeedViewController+TestHelpers.swift
+++ MVVM Module/Tests/Feed UI/Helpers/FeedViewController+TestHelpers.swift
@@ -44,6 +44,10 @@ extension FeedViewController {
 		return errorView.message
 	}
 	
+	func simulateTapOnErrorMessage() {
+		errorView.button.simulateTap()
+	}
+	
 	var isShowingLoadingIndicator: Bool {
 		return refreshControl?.isRefreshing == true
 	}
