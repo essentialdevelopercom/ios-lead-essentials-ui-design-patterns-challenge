d86e4d8944ba1267081d0e900520149555bf171d
Wilmer Barrios <wilmer@grainchain.io>
2021-02-08 18:12:32 -0600

    `FeedViewController` tableView's headerView is `ErrorView`

--- MVVM Module/Source/Feed UI/Controllers/FeedViewController.swift
+++ MVVM Module/Source/Feed UI/Controllers/FeedViewController.swift
@@ -13,10 +13,19 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
 		didSet { tableView.reloadData() }
 	}
 	
+	private lazy var errorView: ErrorView = {
+		return ErrorView()
+	}()
+	
 	public override func viewDidLoad() {
 		super.viewDidLoad()
 		
 		refresh()
+		setupViews()
+	}
+	
+	private func setupViews() {
+		tableView.tableHeaderView = errorView
 	}
 	
 	@IBAction private func refresh() {
--- MVVM Module/Tests/Feed UI/FeedUIIntegrationTests.swift
+++ MVVM Module/Tests/Feed UI/FeedUIIntegrationTests.swift
@@ -281,6 +281,14 @@ final class FeedUIIntegrationTests: XCTestCase {
 		wait(for: [exp], timeout: 1.0)
 	}
 	
+	func test_tableViewHeaderViewIsErrorView() {
+		let (sut, _) = makeSUT()
+		
+		sut.loadViewIfNeeded()
+		
+		XCTAssertTrue(sut.tableView.tableHeaderView is ErrorView)
+	}
+	
 	// MARK: - Helpers
 	
 	private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: FeedViewController, loader: LoaderSpy) {
