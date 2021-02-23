ab2c486a3a8d36b4fd06d4bbd76d2359c27219a5
Erik Agujari <erikagujari@gmail.com>
2021-02-23 13:02:20 +0100

    refactored integration tests on MVP solution to check localized string instead of a not nil message

--- MVP Module/Source/Feed UI/Controllers/FeedViewController.swift
+++ MVP Module/Source/Feed UI/Controllers/FeedViewController.swift
@@ -9,7 +9,7 @@ protocol FeedViewControllerDelegate {
 }
 
 public final class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching, FeedLoadingView, FeedErrorView {
-	@IBOutlet private(set) var errorView: ErrorView?
+	@IBOutlet public private(set) var errorView: ErrorView?
 	var delegate: FeedViewControllerDelegate?
 	
 	private var tableModel = [FeedImageCellController]() {
