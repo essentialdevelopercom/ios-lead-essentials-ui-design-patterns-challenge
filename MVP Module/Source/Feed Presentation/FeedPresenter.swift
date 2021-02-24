29379965c737b44d17e92de856798a4de1aaca64
Erik Agujari <erikagujari@gmail.com>
2021-02-24 11:44:52 +0100

    add missing test for checking that errorView is not visible on reloading the view

--- MVP Module/Source/Feed Presentation/FeedPresenter.swift
+++ MVP Module/Source/Feed Presentation/FeedPresenter.swift
@@ -30,6 +30,7 @@ final class FeedPresenter {
 	
 	func didStartLoadingFeed() {
 		loadingView.display(FeedLoadingViewModel(isLoading: true))
+		errorView.display(FeedErrorStateViewModel(message: nil))
 	}
 	
 	func didFinishLoadingFeed(with feed: [FeedImage]) {
