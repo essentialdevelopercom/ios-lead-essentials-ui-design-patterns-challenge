5ebe649b39213f7b52a6018f2c86710748c52a0f
Ivan <ivan.ornes@gmail.com>
2021-03-01 22:16:26 +0100

    Added `test_loadFeedCompletion_rendersErrorMessageOnErrorUntilNextReload` and `test_errorView_dismissesErrorMessageOnTap` test cases to the MVP UI integration tests Hiding the error message when the viewModel is loading in the MVP Module

--- MVP Module/Source/Feed UI/Controllers/FeedViewController.swift
+++ MVP Module/Source/Feed UI/Controllers/FeedViewController.swift
@@ -29,6 +29,7 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
 	
 	func display(_ viewModel: FeedLoadingViewModel) {
 		if viewModel.isLoading {
+			errorView?.hideMessage()
 			refreshControl?.beginRefreshing()
 		} else {
 			refreshControl?.endRefreshing()
