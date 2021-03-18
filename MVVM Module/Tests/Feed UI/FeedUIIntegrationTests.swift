aec34a38295b2b8923fac81e888e6eb1c60ff9ca
Mo Al Waili <mohammed.alwaili@gmail.com>
2021-03-19 00:15:42 +0100

    test 'errorView' dismiss error message on tap

--- MVVM Module/Tests/Feed UI/FeedUIIntegrationTests.swift
+++ MVVM Module/Tests/Feed UI/FeedUIIntegrationTests.swift
@@ -87,6 +87,19 @@ final class FeedUIIntegrationTests: XCTestCase {
 		loader.completeFeedLoadingWithError(at: 0)
 		XCTAssertEqual(sut.errorMessage, localized("FEED_VIEW_CONNECTION_ERROR"))
 		
+		sut.simulateUserInitiatedFeedReload()
+		XCTAssertEqual(sut.errorMessage, nil)
+	}
+	
+	func test_errorView_dismissesErrorMessageOnTap() {
+		let (sut, loader) = makeSUT()
+		
+		sut.loadViewIfNeeded()
+		XCTAssertEqual(sut.errorMessage, nil)
+		
+		loader.completeFeedLoadingWithError(at: 0)
+		XCTAssertEqual(sut.errorMessage, localized("FEED_VIEW_CONNECTION_ERROR"))
+		
 		sut.simulateTapOnErrorMessage()
 		XCTAssertEqual(sut.errorMessage, nil)
 	}
