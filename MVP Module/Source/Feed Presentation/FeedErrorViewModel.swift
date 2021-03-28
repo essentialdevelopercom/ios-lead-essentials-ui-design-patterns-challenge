65a3631003cddf11e8ec86f6a27bc73b058d3eba
john roque jorillo <jorillojohnroque@gmail.com>
2021-03-29 02:38:12 +0800

    Connect Storyboard ErrorView & Implement mvp components for errorView

---
+++ MVP Module/Source/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedErrorViewModel.swift
+//  MVP
+//
+//  Created by John Roque Jorillo on 3/29/21.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+struct FeedErrorViewModel {
+	let errorMessage: String?
+}
