0c06985346e2d32c2e62445244acb5e0ef0e4561
Romain <rbrunie@cityway.fr>
2021-03-30 18:16:19 +0200

    Hide error view message on tap

--- MVVM Module/Tests/Feed UI/FeedUIIntegrationTests.swift
+++ MVVM Module/Tests/Feed UI/FeedUIIntegrationTests.swift
@@ -92,16 +92,16 @@ final class FeedUIIntegrationTests: XCTestCase {
 	}
 	
 	func test_errorView_dismissesErrorMessageOnTap() {
-//		let (sut, loader) = makeSUT()
-//
-//		sut.loadViewIfNeeded()
-//		XCTAssertEqual(sut.errorMessage, nil)
-//
-//		loader.completeFeedLoadingWithError(at: 0)
-//		XCTAssertEqual(sut.errorMessage, localized("FEED_VIEW_CONNECTION_ERROR"))
-//
-//		sut.simulateTapOnErrorMessage()
-//		XCTAssertEqual(sut.errorMessage, nil)
+		let (sut, loader) = makeSUT()
+
+		sut.loadViewIfNeeded()
+		XCTAssertEqual(sut.errorMessage, nil)
+
+		loader.completeFeedLoadingWithError(at: 0)
+		XCTAssertEqual(sut.errorMessage, localized("FEED_VIEW_CONNECTION_ERROR"))
+
+		sut.simulateTapOnErrorMessage()
+		XCTAssertEqual(sut.errorMessage, nil)
 	}
 	
 	func test_feedImageView_loadsImageURLWhenVisible() {
