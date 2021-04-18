cfda85fe9aa7f2dcba84186fed694ebe0f3d4095
danielmoro <daniel@ezderm.com>
2021-04-18 11:36:01 +0200

    Feed view does not show error message on show

--- MVVM Module/Tests/Feed UI/FeedUIIntegrationTests.swift
+++ MVVM Module/Tests/Feed UI/FeedUIIntegrationTests.swift
@@ -280,6 +280,14 @@ final class FeedUIIntegrationTests: XCTestCase {
 		wait(for: [exp], timeout: 1.0)
 	}
 
+	func test_feedView_doesNotShowErrorMessageOnShow() {
+		let (sut, _) = makeSUT()
+
+		sut.loadViewIfNeeded()
+
+		XCTAssertEqual(sut.errorMessage, nil, "Expected no error messages when view is just loaded")
+	}
+
 	// MARK: - Helpers
 
 	private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: FeedViewController, loader: LoaderSpy) {
