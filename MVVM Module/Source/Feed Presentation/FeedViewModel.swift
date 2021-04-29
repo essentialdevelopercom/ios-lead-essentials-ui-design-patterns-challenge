19dd26fa7a931053c9def10d78a2cbe34119fa29
Nadheer Chatharoo <nchatharoo@icloud.com>
2021-04-29 18:42:18 +0200

    MVVM - Display error message on completion error until next reload

--- MVVM Module/Source/Feed Presentation/FeedViewModel.swift
+++ MVVM Module/Source/Feed Presentation/FeedViewModel.swift
@@ -24,6 +24,7 @@ final class FeedViewModel {
 
 	func loadFeed() {
 		onLoadingStateChange?(true)
+		onErrorState?(.none)
 		feedLoader.load { [weak self] result in
 			if let feed = try? result.get() {
 				self?.onFeedLoad?(feed)
