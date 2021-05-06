48a4deaa44717c429da4db2f0e5e23848c9e3450
Mikhail Macnev <misha@macnev.ru>
2021-05-06 14:58:59 +0500

    (MVP) improved implementation for error displays, decoupling the implementation of `FeedErrorViewModel` and `FeedLoadingViewModel`

--- MVP Module/Source/Feed UI/Controllers/FeedViewController.swift
+++ MVP Module/Source/Feed UI/Controllers/FeedViewController.swift
@@ -8,7 +8,7 @@ protocol FeedViewControllerDelegate {
 	func didRequestFeedRefresh()
 }
 
-public final class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching, FeedLoadingView {
+public final class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching, FeedLoadingView, FeedErrorView {
 	@IBOutlet private(set) var errorView: ErrorView?
 
 	var delegate: FeedViewControllerDelegate?
@@ -33,7 +33,9 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
 		} else {
 			refreshControl?.endRefreshing()
 		}
+	}
 
+	func display(_ viewModel: FeedErrorViewModel) {
 		if let errorMessage = viewModel.errorMessage {
 			errorView?.show(message: errorMessage)
 		} else {
