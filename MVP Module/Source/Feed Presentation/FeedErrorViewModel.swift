4baafe40183bf22d83afe0c89ab73081acbb6aa2
Maciej Krolikowski <maciej.krolikowski@allegro.pl>
2021-05-12 18:59:23 +0200

    Move FeedErrorViewModel to separate file

---
+++ MVP Module/Source/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedErrorViewModel.swift
+//  MVP
+//
+//  Created by Maciej Krolikowski on 12/05/2021.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+struct FeedErrorViewModel {
+	let message: String
+}
