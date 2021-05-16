f156882bd841d1bbd3816dd5d6922fc10a60ff76
Danil Lakhtin <danil_lahtin@mail.ru>
2021-05-16 19:34:10 +0300

    Add FeedErrorViewModel

---
+++ MVP Module/Source/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  FeedErrorViewModel.swift
+//  MVP
+//
+//  Created by Danil Lahtin on 16.05.2021.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+struct FeedErrorViewModel {
+	let message: String?
+}
