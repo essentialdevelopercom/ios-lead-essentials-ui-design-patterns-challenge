9f572f7fc422c9074f1419949751d45b562e21a2
Vladimir Jeremic <vladimir@ezderm.com>
2021-05-19 12:32:10 +0200

    added FeedErrorViewModel to separate file

---
+++ MVP Module/Source/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  FeedErrorViewModel.swift
+//  MVP
+//
+//  Created by Vladimir Jeremic on 5/19/21.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+struct FeedErrorViewModel {
+	let errorMessage: String?
+}
