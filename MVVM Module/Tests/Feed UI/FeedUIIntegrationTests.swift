5ec74fb40e4b68d5d5f500f25d5cacdc81688f98
Tiago Amorim <tamorim@smithmicro.com>
2021-05-20 08:50:13 +0100

    MVVM - Combine unit tests to improve readability.

--- MVVM Module/Tests/Feed UI/FeedUIIntegrationTests.swift
+++ MVVM Module/Tests/Feed UI/FeedUIIntegrationTests.swift
@@ -265,39 +265,30 @@ final class FeedUIIntegrationTests: XCTestCase {
 		wait(for: [exp], timeout: 1.0)
 	}
 
-	func test_loadFeedCompletion_showsErrorOnLoadFail() {
+	func test_loadFeedCompletionWithError_showsErrorMessageUntilNextReload() {
 		let (sut, loader) = makeSUT()
+
 		sut.loadViewIfNeeded()
+		XCTAssertEqual(sut.errorMessage, nil)
 
 		loader.completeFeedLoadingWithError()
-
-		XCTAssertEqual(sut.errorMessage, localized("FEED_VIEW_CONNECTION_ERROR"), "Expect error message when load fails")
-	}
-
-	func test_loadFeedCompletion_hideErrorMessageOnReload() {
-		let (sut, loader) = makeSUT()
-		sut.loadViewIfNeeded()
-
-		loader.completeFeedLoadingWithError()
-
-		XCTAssertEqual(sut.errorMessage, localized("FEED_VIEW_CONNECTION_ERROR"), "Expect error message when load fails")
+		XCTAssertEqual(sut.errorMessage, localized("FEED_VIEW_CONNECTION_ERROR"))
 
 		sut.simulateUserInitiatedFeedReload()
-
-		XCTAssertEqual(sut.errorMessage, nil, "No error message is expected when reaload starts")
+		XCTAssertEqual(sut.errorMessage, nil)
 	}
 
-	func test_ErrorView_dismissedErrorMessageOnTap() {
+	func test_errorView_dismissesErrorMessageOnTap() {
 		let (sut, loader) = makeSUT()
+
 		sut.loadViewIfNeeded()
+		XCTAssertEqual(sut.errorMessage, nil)
 
 		loader.completeFeedLoadingWithError()
-
-		XCTAssertEqual(sut.errorMessage, localized("FEED_VIEW_CONNECTION_ERROR"), "Expect error message when load fails")
+		XCTAssertEqual(sut.errorMessage, localized("FEED_VIEW_CONNECTION_ERROR"))
 
 		sut.simulateTapOnErrorMessage()
-
-		XCTAssertEqual(sut.errorMessage, nil, "No error message is expected when reaload starts")
+		XCTAssertEqual(sut.errorMessage, nil)
 	}
 
 	func test_loadImageDataCompletion_dispatchesFromBackgroundToMainThread() {
