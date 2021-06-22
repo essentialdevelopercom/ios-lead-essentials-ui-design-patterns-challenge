e1997e27d571e91d30e5cb2149d862800631311c
Walid <sassi.walid@outlook.com>
2021-06-22 21:21:00 +0200

    combine loadFeedCompletion error view tests  in one test

--- MVVM Module/Tests/Feed UI/FeedUIIntegrationTests.swift
+++ MVVM Module/Tests/Feed UI/FeedUIIntegrationTests.swift
@@ -280,26 +280,17 @@ final class FeedUIIntegrationTests: XCTestCase {
 		wait(for: [exp], timeout: 1.0)
 	}
 
-	func test_loadFeedCompletion_rendersErrorMessage() {
+	func test_loadFeedCompletionWithError_showsErrorMessageUntilNextReload() {
 		let (sut, loader) = makeSUT()
 
 		sut.loadViewIfNeeded()
+		XCTAssertEqual(sut.errorMessage, nil)
 
 		loader.completeFeedLoadingWithError()
 		XCTAssertEqual(sut.errorMessage, localized("FEED_VIEW_CONNECTION_ERROR"))
-	}
-
-	func test_loadFeedCompletion_hideErrorMessageOnReload() {
-		let (sut, loader) = makeSUT()
-
-		sut.loadViewIfNeeded()
-
-		loader.completeFeedLoadingWithError()
-		XCTAssertEqual(sut.errorMessage, localized("FEED_VIEW_CONNECTION_ERROR"), "Expect error message on fails")
 
 		sut.simulateUserInitiatedFeedReload()
-
-		XCTAssertEqual(sut.errorMessage, nil, "No error message is expected when reaload starts")
+		XCTAssertEqual(sut.errorMessage, nil)
 	}
 
 	func test_errorView_dismissedErrorMessageOnTap() {
