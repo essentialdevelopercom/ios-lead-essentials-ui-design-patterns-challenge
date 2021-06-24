8cf49b22ab20689f1e4104f7cffd64b8ace5ffe2
Naveen <naveentg.ntg@gmail.com>
2021-06-24 18:44:53 +0530

    Handled new onLoadError on the FeedViewModel with integrating with Error View along with integration tests

--- MVVM Module/Source/Feed UI/Controllers/FeedViewController.swift
+++ MVVM Module/Source/Feed UI/Controllers/FeedViewController.swift
@@ -5,6 +5,8 @@
 import UIKit
 
 public final class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching {
+	@IBOutlet private var errorView: ErrorView!
+
 	var viewModel: FeedViewModel? {
 		didSet { bind() }
 	}
@@ -30,6 +32,13 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
 				self?.refreshControl?.beginRefreshing()
 			} else {
 				self?.refreshControl?.endRefreshing()
+			}
+		}
+		viewModel?.onLoadError = { [weak self] message in
+			if let message = message {
+				self?.errorView?.show(message: message)
+			} else {
+				self?.errorView?.hideMessage()
 			}
 		}
 	}
