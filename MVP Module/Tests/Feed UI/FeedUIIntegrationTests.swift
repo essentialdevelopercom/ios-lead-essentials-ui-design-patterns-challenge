a6693c050bf6d551085d2a72533b19a859f4bc17
Tan Tran <tantan39>
2021-06-25 14:57:03 -0400

    [MVP] Dismiss error message on tap view

--- MVP Module/Tests/Feed UI/FeedUIIntegrationTests.swift
+++ MVP Module/Tests/Feed UI/FeedUIIntegrationTests.swift
@@ -299,6 +299,16 @@ final class FeedUIIntegrationTests: XCTestCase {
 		XCTAssertNil(sut.errorMessage)
 	}
 
+	func test_loadFeedCompletion_dismissErrorMessageOnTap() {
+		let (sut, loader) = makeSUT()
+
+		sut.loadViewIfNeeded()
+		loader.completeFeedLoadingWithError()
+		sut.errorView.button.simulateTap()
+
+		XCTAssertNil(sut.errorMessage)
+	}
+
 	// MARK: - Helpers
 
 	private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: FeedViewController, loader: LoaderSpy) {
