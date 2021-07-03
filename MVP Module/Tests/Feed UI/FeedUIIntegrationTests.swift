0eb6326af1dac26ef49182a097344fb86ccef3d2
224Apps <224apps@gmail.com>
2021-07-03 14:04:55 -0400

    MVP - feedView does not render error message when view is loaded

--- MVP Module/Tests/Feed UI/FeedUIIntegrationTests.swift
+++ MVP Module/Tests/Feed UI/FeedUIIntegrationTests.swift
@@ -280,6 +280,13 @@ final class FeedUIIntegrationTests: XCTestCase {
 		wait(for: [exp], timeout: 1.0)
 	}
 
+	func test_feedView_doesNotRendersErrorMessageOnViewLoad() {
+		let (sut, _) = makeSUT()
+
+		sut.loadViewIfNeeded()
+		XCTAssertNil(sut.errorMessage)
+	}
+
 	// MARK: - Helpers
 
 	private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: FeedViewController, loader: LoaderSpy) {
