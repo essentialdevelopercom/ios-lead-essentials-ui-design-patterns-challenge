4028f3a447e515fe20b2dc32c7b7b1f2e6530fd3
Matthias Sehrbrock <matthias.sehrbrock@grandcentrix.net>
2021-07-06 10:04:08 +0200

    MVP - use .none instead of nil for no errorMessage to be consistent

--- MVP Module/Source/Feed Presentation/FeedPresenter.swift
+++ MVP Module/Source/Feed Presentation/FeedPresenter.swift
@@ -31,7 +31,7 @@ final class FeedPresenter {
 	}
 
 	func didStartLoadingFeed() {
-		errorView.display(FeedErrorViewModel(errorMessage: nil))
+		errorView.display(FeedErrorViewModel(errorMessage: .none))
 		loadingView.display(FeedLoadingViewModel(isLoading: true))
 	}
 
