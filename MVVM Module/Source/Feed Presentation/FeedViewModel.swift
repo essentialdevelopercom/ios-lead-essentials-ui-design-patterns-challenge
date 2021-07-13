2e900273af3059d29448a8219feb8593c1ab8092
Fabio Mignogna <mig.fabio@gmail.com>
2021-07-13 23:39:23 +0100

    MVVM: Hides error message on reload feed

--- MVVM Module/Source/Feed Presentation/FeedViewModel.swift
+++ MVVM Module/Source/Feed Presentation/FeedViewModel.swift
@@ -20,10 +20,11 @@ final class FeedViewModel {
 
 	var onLoadingStateChange: Observer<Bool>?
 	var onFeedLoad: Observer<[FeedImage]>?
-	var onFeedLoadError: Observer<String>?
+	var onFeedLoadError: Observer<String?>?
 
 	func loadFeed() {
 		onLoadingStateChange?(true)
+		onFeedLoadError?(nil)
 		feedLoader.load { [weak self] result in
 			switch result {
 			case .success(let feed):
--- MVVM Module/Source/Feed UI/Controllers/FeedViewController.swift
+++ MVVM Module/Source/Feed UI/Controllers/FeedViewController.swift
@@ -35,7 +35,11 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
 			}
 		}
 		viewModel?.onFeedLoadError = { [weak self] errorMessage in
-			self?.errorView.show(message: errorMessage)
+			if let errorMessage = errorMessage {
+				self?.errorView.show(message: errorMessage)
+			} else {
+				self?.errorView.hideMessage()
+			}
 		}
 	}
 
