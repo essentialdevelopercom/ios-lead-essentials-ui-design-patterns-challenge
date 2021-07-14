6044a54bd4973d4aba044206042620b8b4f7e149
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2021-07-14 07:45:07 +0300

    Done with MVP module - add errorView, functions

---
+++ MVP Module/Source/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  FeedErrorViewModel.swift
+//  MVP
+//
+//  Created by OLEKSANDR ISAIEV on 13.07.2021.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+struct FeedErrorViewModel {
+	let errorMessage: String?
+}
